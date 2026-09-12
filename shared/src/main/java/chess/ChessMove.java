fe3207438d6372b54197ce9322325a01dd6312a4
MojoDallin <dallin.t.schmidt@gmail.com>
2026-09-12 13:28:27 -0600

    added StartPosition, EndPosition, PromotionPiece fields; implemented getStartPosition, getEndPosition, getPromotionPiece methods; overrode equals and hashcode methods

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -8,23 +10,26 @@ package chess;
  */
 public class ChessMove {
 
-    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
-                     ChessPiece.PieceType promotionPiece) {
+    private final ChessPosition StartPosition;
+    private final ChessPosition EndPosition;
+    private final ChessPiece.PieceType PromotionPiece;
+
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece)
+    {
+        StartPosition = startPosition;
+        EndPosition = endPosition;
+        PromotionPiece = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
-    public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
-    }
+    public ChessPosition getStartPosition() { return StartPosition; }
 
     /**
      * @return ChessPosition of ending location
      */
-    public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
-    }
+    public ChessPosition getEndPosition() { return EndPosition; }
 
     /**
      * Gets the type of piece to promote a pawn to if pawn promotion is part of this
@@ -32,7 +37,28 @@ public class ChessMove {
      *
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
-    public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+    public ChessPiece.PieceType getPromotionPiece() { return PromotionPiece; }
+
+    /**
+     * Compares the chess move against another object to determine if they are equal.
+     * @param o The reference object with which to compare.
+     * @return True if the objects are the same, False if the objects are different.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(StartPosition, chessMove.StartPosition) && Objects.equals(EndPosition, chessMove.EndPosition) && PromotionPiece == chessMove.PromotionPiece;
+    }
+
+    /**
+     * Generates a hashcode consisting of StartPosition, EndPosition, and Promotion Piece.
+     * @return The generated hashcode.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(StartPosition, EndPosition, PromotionPiece);
     }
 }
