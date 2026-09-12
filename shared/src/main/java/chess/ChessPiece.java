b2ffe697cc7490c7d2f4534c5bf70bfd5b781519
MojoDallin <dallin.t.schmidt@gmail.com>
2026-09-12 16:20:50 -0600

    added framework for the rest of the pieces; moved rook movement to separate function; added king movement; added rook and king movement to queen movement

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,9 +1,6 @@
 package chess;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * Represents a single chess piece
@@ -60,7 +57,7 @@ public class ChessPiece {
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition)
     {
-        List<ChessMove> possibleMoves = new ArrayList<>();
+        HashSet<ChessMove> possibleMoves = new HashSet<>(); // HashSet because we don't want duplicate positions, and they do not need to be ordered
         int currentRow = myPosition.getRow();
         int currentCol = myPosition.getColumn();
 
@@ -90,31 +87,92 @@ public class ChessPiece {
             }
             case ROOK:
             {
-                for(int i = -1; i < 2; i += 2) // vertical
-                {
-                    int nextPos = currentRow + i;
-                    ChessPosition newPosition = board.getPositionAt(nextPos, currentCol);
-                    while (newPosition != null && newPosition.getCurrentPiece() == null) // next position is in bounds and not occupied
-                    {
-                        possibleMoves.add(new ChessMove(myPosition, newPosition, null));
-                        nextPos += i;
-                        newPosition = board.getPositionAt(nextPos, currentCol);
-                    }
-                }
-                for(int i = -1; i < 2; i += 2) // horizontal
-                {
-                    int nextPos = currentCol + i;
-                    ChessPosition newPosition = board.getPositionAt(nextPos, currentCol);
-                    while(newPosition != null && newPosition.getCurrentPiece() != null)
-                    {
-                        possibleMoves.add(new ChessMove(myPosition, newPosition, null));
-                        nextPos += i;
-                        newPosition = board.getPositionAt(nextPos, currentCol);
-                    }
-                }
+                possibleMoves.addAll(getHorizontalAndVerticalMoves(board, myPosition));
+            }
+            case KNIGHT:
+            {
+
+            }
+            case BISHOP:
+            {
+
+            }
+            case KING:
+            {
+                possibleMoves.addAll(getEightMovesCenteredOnPiece(board, myPosition));
+            }
+            case QUEEN:
+            {
+                possibleMoves.addAll(getEightMovesCenteredOnPiece(board, myPosition)); // king moves
+                possibleMoves.addAll(getHorizontalAndVerticalMoves(board, myPosition)); // rook moves
             }
         }
+
         return possibleMoves;
+    }
+
+    /**
+     * Grabs the 8 closest positions around the piece, centered on itself. Intended for use with King and Queen movement.
+     * @param board The chess board.
+     * @param oldPosition The 'old', or current position of the piece to calculate moves for.
+     * @return A list of ChessMove(s) centered on the current piece.
+     */
+    private HashSet<ChessMove> getEightMovesCenteredOnPiece(ChessBoard board, ChessPosition oldPosition) // for king and queen
+    {
+        HashSet<ChessMove> centeredMoves = new HashSet<>();
+
+        for(int i = 1; i > -1; i--)
+            for(int j = 1; j > -1; j--)
+            {
+                int newRow = oldPosition.getRow() - i;
+                int newCol = oldPosition.getColumn() - i;
+                if(newRow > 0 && newRow < 9 && newCol > 0 && newCol < 9) // both in bounds
+                {
+                    ChessPosition newPosition = board.getPositionAt(newRow, newCol);
+                    if(!oldPosition.equals(newPosition) && newPosition.getCurrentPiece() == null) // do not add current square to possible moves and do not add occupied pieces
+                        centeredMoves.add(new ChessMove(oldPosition, newPosition, null));
+                }
+            }
+
+        return centeredMoves;
+    }
+
+    /**
+     * Grabs all the horizontal and vertical spaces a piece can move to. Intended for use with Rook and Queen movement.
+     * @param board The chess board.
+     * @param oldPosition The 'old', or current position of the piece to calculate moves for.
+     * @return A list of ChessMove(s) centered on the current piece.
+     */
+    private HashSet<ChessMove> getHorizontalAndVerticalMoves(ChessBoard board, ChessPosition oldPosition) // for rook and queen
+    {
+        HashSet<ChessMove> verticalHorizontalMoves = new HashSet<>();
+        int currentCol = oldPosition.getColumn();
+
+        for(int i = -1; i < 2; i += 2) // vertical
+        {
+            int nextPos = oldPosition.getRow() + i;
+            ChessPosition newPosition = board.getPositionAt(nextPos, currentCol);
+            while (newPosition != null && newPosition.getCurrentPiece() == null) // next position is in bounds and not occupied
+            {
+                verticalHorizontalMoves.add(new ChessMove(oldPosition, newPosition, null));
+                nextPos += i;
+                newPosition = board.getPositionAt(nextPos, currentCol);
+            }
+        }
+
+        for(int i = -1; i < 2; i += 2) // horizontal
+        {
+            int nextPos = currentCol + i;
+            ChessPosition newPosition = board.getPositionAt(nextPos, currentCol);
+            while(newPosition != null && newPosition.getCurrentPiece() != null)
+            {
+                verticalHorizontalMoves.add(new ChessMove(oldPosition, newPosition, null));
+                nextPos += i;
+                newPosition = board.getPositionAt(nextPos, currentCol);
+            }
+        }
+
+        return verticalHorizontalMoves;
     }
 
     /**
