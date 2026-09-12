a9af75f16fe10eb4480f03046f1fea94d5eb90da
MojoDallin <dallin.t.schmidt@gmail.com>
2026-09-11 22:37:20 -0600

    added CurrentPiece field, separate constructor for adding a new position with piece methods for getting, adding, and removing piece; improved comparisons to use CurrentPiece in calculations

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -12,11 +12,19 @@ public class ChessPosition {
 
     private final int Row;
     private final int Column;
+    private ChessPiece CurrentPiece = null;
 
     public ChessPosition(int row, int col)
     {
         Row = row;
         Column = col;
+    }
+
+    public ChessPosition(int row, int col, ChessPiece piece)
+    {
+        Row = row - 1; // - 1 to account for non-zero indexing
+        Column = col - 1;
+        CurrentPiece = piece;
     }
 
     /**
@@ -38,6 +46,23 @@ public class ChessPosition {
     }
 
     /**
+     * Gets the current chess piece on the position, if there is any.
+     * @return The ChessPiece object that is currently on the object; if there is no piece, then returns null.
+     */
+    public ChessPiece getCurrentPiece() { return CurrentPiece; }
+
+    /**
+     * Adds a chess piece to the position. Does NOT check if piece is currently occupied.
+     * @param newPiece The piece to add.
+     */
+    public void addPiece(ChessPiece newPiece) { CurrentPiece = newPiece; }
+
+    /**
+     * Removes the current chess piece from the position.
+     */
+    public void removePiece() { CurrentPiece = null; }
+
+    /**
      * Compares the chess position against another object to determine if they are equal.
      * @param o The reference object with which to compare.
      * @return True if the objects are the same, False if the objects are different.
@@ -49,16 +74,16 @@ public class ChessPosition {
             return false;
         }
         ChessPosition that = (ChessPosition) o;
-        return Row == that.Row && Column == that.Column;
+        return Row == that.Row && Column == that.Column && Objects.equals(CurrentPiece, that.CurrentPiece);
     }
 
     /**
-     * Generates a hashcode consisting of Row and Column.
+     * Generates a hashcode consisting of Row, Column, and CurrentPiece.
      * @return The generated hashcode.
      */
     @Override
     public int hashCode()
     {
-        return Objects.hash(Row, Column);
+        return Objects.hash(Row, Column, CurrentPiece);
     }
 }
