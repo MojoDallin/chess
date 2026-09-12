3227b40f2305cb6916b1382c20433d1f7fed0f1a
MojoDallin <dallin.t.schmidt@gmail.com>
2026-09-11 20:36:28 -0600

    added equals and hashCode methods, along with Row and Column fields (private)

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -8,22 +10,55 @@ package chess;
  */
 public class ChessPosition {
 
-    public ChessPosition(int row, int col) {
+    private final int Row;
+    private final int Column;
+
+    public ChessPosition(int row, int col)
+    {
+        Row = row;
+        Column = col;
     }
 
     /**
      * @return which row this position is in
      * 1 codes for the bottom row
      */
-    public int getRow() {
-        throw new RuntimeException("Not implemented");
+    public int getRow()
+    {
+        return Row;
     }
 
     /**
      * @return which column this position is in
      * 1 codes for the left column
      */
-    public int getColumn() {
-        throw new RuntimeException("Not implemented");
+    public int getColumn()
+    {
+        return Column;
+    }
+
+    /**
+     * Compares the chess position against another object to determine if they are equal.
+     * @param o The reference object with which to compare.
+     * @return True if the objects are the same, False if the objects are different.
+     */
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPosition that = (ChessPosition) o;
+        return Row == that.Row && Column == that.Column;
+    }
+
+    /**
+     * Generates a hashcode consisting of Row and Column.
+     * @return The generated hashcode.
+     */
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(Row, Column);
     }
 }
